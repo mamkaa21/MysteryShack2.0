f7362835d0167c0fb40b3c9b4645995e17fcee66
mamka <mamka21a@mail.ru>
2025-02-07 15:50:23 +1000

    надо сделать апи апи (вернусь часов в 5)

---
+++ MysteryShack/DB.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MysteryShack
+{
+    public class DB
+    {
+        private static DB instance;
+
+        HttpClient client = new HttpClient();
+
+        private List<Goods> Goods { get; set; }
+        private List<Category> Categories { get; set; }
+        private Goods Good { get; set; }
+        private Category Category { get; set; }
+
+        public static DB GetInstance()
+        {
+            if (instance == null)
+                instance = new DB();
+            return instance;
+        }
+
+        public DB()
+        {
+            client.BaseAddress = new Uri("");
+        }
+
+        public async Task<List<Goods>> GetListGoods()
+        {
+            var responce = await client.GetAsync($"Resignationletters/GetResignationletters");
+            if (responce.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                var result = await responce.Content.ReadAsStringAsync();
+                await Application.Current.MainPage.DisplayAlert("Оши1бка", $"{result}", "ок");
+                return null;
+            }
+            else
+            {
+                var letters = await responce.Content.ReadFromJsonAsync<List<Goods>>();
+                foreach (var item in letters)
+                {
+                    item.SetStudent();
+                }
+                return letters;
+            }
+        }
+
+        public async Task<List<Category>> GetListCategory()
+        {
+            var responce = await client.GetAsync($"Students/GetStudents");
+            if (responce.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                var result = await responce.Content.ReadAsStringAsync();
+                await Application.Current.MainPage.DisplayAlert("О2шибка", $"{result}", "ок");
+                return null;
+            }
+            else
+            {
+                var students = await responce.Content.ReadFromJsonAsync<List<Student>>();
+                return students;
+            }
+        }
+    }
+}
--- MysteryShack/MainWindow.xaml.cs
+++ MysteryShack/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,17 +9,67 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.EntityFrameworkCore;
 
 namespace MysteryShack
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+       
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private Category category;
+        public Category Category
+        {
+            get => category;
+            set
+            {
+                category = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Category)));
+            }
+        }
+
+        private List<Category> categories;
+        public List<Category> Categories
+        {
+            get => categories;
+            set
+            {
+                categories = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Categories)));
+            }
+        }
+
+        private Goods good;
+        public Goods Good
+        {
+            get => good;
+            set
+            {
+                good = value;
+                PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(nameof(Good)));
+            }
+        }
+
+        private List<Goods> goods;
+        public List<Goods> Goods
+        {
+            get => goods;
+            set
+            {
+                goods = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Goods)));
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = this;
         }
 
         private void ShowForm(object sender, RoutedEventArgs e)
